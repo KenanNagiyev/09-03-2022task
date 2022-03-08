4b11c235a61230f375530f099d072065758d6eb6
KenanNagiyev <788lnmp@code.edu.az>
2022-03-09 00:38:31 +0400

    new file

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Text-i daxil edin");
+            string text = Console.ReadLine();
+            Console.WriteLine(VowelLetter(text));
+        }
+        static int VowelLetter(string text)
+        {
+            char[] letter = { 'a', 'u', 'e', 'o', 'i' };
+            int count = 0;
+            for (int i = 0; i < text.Length; i++)
+                for (int j = 0; j < letter.Length; j++)
+                {
+
+                    if (text[i] == letter[j])
+                    {
+                        count++;
+                    }
+                }
+            return count;
+        }
+    }
+}
